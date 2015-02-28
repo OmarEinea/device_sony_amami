58712203da2e830cf0be491803b69d8902ebcb61
Omar Einea <eineao@gmail.com>
2015-02-28 12:28:56 +0400

    amami: Omnify device tree
    Signed-off-by: Omar Einea <eineao@gmail.com>

--- omni_amami.mk
+++ omni_amami.mk
@@ -1,12 +1,13 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit Omni product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
+# Inherit device configuration
 $(call inherit-product, device/sony/amami/full_amami.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5503 BUILD_FINGERPRINT=Sony/D5503/D5503:4.4.4/14.4.A.0.157/ev__jQ:user/release-keys PRIVATE_BUILD_DESC="D5503-user 4.4.4 14.4.A.0.157 ev__jQ release-keys"
 
-PRODUCT_NAME := cm_amami
+PRODUCT_NAME := omni_amami
 PRODUCT_DEVICE := amami
